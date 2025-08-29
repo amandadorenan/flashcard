0e8c3144d0de89894aed40d2917464f37503cbb9
Amanda scheffer <132097947+amandadorenan@users.noreply.github.com>
2025-08-29 16:04:36 -0300

    Add files via upload

---
+++ flashcard Amanda Tavares Scheffer/assets/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'geografia',
+    'Qual é a maior montanha do mundo?',
+    'A montanha mais alta do mundo é o Monte Everest',
+)
+
+criaCartao(
+    'geografia',
+    'Qual é o nome do oceano que se encontra entre a África e a Europa?',
+    'Oceano Atlântico.',
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin',
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Que país tem o formato de uma bota?',
+    ' Itália',
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Quantos ossos temos no nosso corpo?',
+    '206 ossos',
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Qual a especialidade do otorrinolaringologista?',
+    'Tratamento das doenças relacionadas a nariz, ouvido e garganta',
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Qual a nacionalidade do sociólogo e filósofo Durkheim?',
+    'Francesa',
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Depois do futebol, qual o esporte mais popular no Brasil?',
+    'Vôlei',
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Que cidade brasileira é conhecida por chover todos os dias quase à mesma hora?',
+    'Belém',
+)
